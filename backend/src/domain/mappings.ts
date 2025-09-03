6745c901407c37da5e6420e7e6c33a950ee2d54e
Lucas Alves Leandro <Lucas.yumi632@gmail.com>
2025-09-03 13:21:56 -0300

    feat: definindo modelos de dominio e interface
    definindo modelos de domínio e interfaces TypeScript para as ofertas.

---
+++ backend/src/domain/mappings.ts
@@ -0,0 +1,22 @@
+export const KIND_MAPPINGS = {
+    'Presencial' : 'Presencial',
+    'EaD' : 'EaD'
+} as const;
+
+export const LEVEL_MAPPINGS  = {
+    'bacharelado': 'Graduação (bacharelado)',
+    'tecnologo': 'Graduação (tecnólogo)',
+    'licenciatura': 'Graduação (licenciatura)'
+} as const;
+
+export const formatCurrency = (value: number) => {
+    return new Intl.NumberFormat('pt-BR',{
+        style: 'currency',
+        currency : 'BRL'
+    }).format(value);
+};
+
+export const calculateDiscountPercentage = (fullPrice:number, offeredPrice : number) : string => {
+    const discount = Math.round(((fullPrice - offeredPrice)/fullPrice)*100);
+    return `${discount}%`
+}
---
+++ backend/src/domain/offer.ts
@@ -0,0 +1,46 @@
+export interface RawOffer {
+   courseName: string,
+    rating: number,
+    fullPrice: number,
+    offeredPrice: number,
+    kind: string ,
+    level: string,
+    iesLogo: string,
+    iesName: string
+}
+
+export interface ProcessedOffer {
+    courseName: string,
+    rating: number,
+    fullPrice: number,
+    offeredPrice: number,
+    kind: string ,
+    level: string,
+    iesLogo: string,
+    iesName: string,
+    discountPorcentage: string
+}
+
+export interface QueryFilters{
+    level?: ('bacharelado' | 'tecnologo' | 'licenciatura')[];
+    kind?: ('Presencial' | 'EaD')[];
+    minPrice?: number;
+    maxPrice?: number;
+    search?: string;
+    sortBy?: 'courseName' | 'offeredPrice' | 'rating';
+    sortOrder?: 'asc' | 'desc';
+    page?: number;
+    limit?: number;
+    fields?: (keyof ProcessedOffer)[];
+
+}
+
+export interface PaginatedResponse<T> {
+  data: T[];
+  pagination: {
+    currentPage: number;
+    totalPages: number;
+    totalItems: number;
+    itemsPerPage: number;
+  };
+}
